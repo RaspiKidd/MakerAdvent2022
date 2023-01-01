44f494ca12aad9e6eb1183e6ad66fa3128e390f4
Kerry Kidd <kerry@raspikidd.com>
2023-01-01 17:07:58 +0000

    Day 12 Complete - LED Strip

---
+++ LEDStrip02.py
@@ -0,0 +1,19 @@
+# Write your code here :-)
+# Imports
+import time
+from machine import Pin
+from neopixel import NeoPixel
+
+# Define the strip pin number (28) and number of LEDs (15)
+strip = NeoPixel(Pin(28), 15)
+
+# Select the first pixel (pixel 0)
+# Set the RGB colour (red)
+strip[0] = (255,0,0)
+strip[1] = (255,0,0)
+strip[2] = (255,0,0)
+strip[3] = (255,0,0)
+strip[4] = (255,0,0)
+
+# Send the data to the strip
+strip.write()
